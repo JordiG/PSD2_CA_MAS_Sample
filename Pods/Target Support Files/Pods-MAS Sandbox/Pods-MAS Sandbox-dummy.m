a1d92c9fd15591e689771f087017521aa6c8782c
Jordi Gascon <j_gascon@telefonica.net>
2017-07-03 18:28:17 +0200

    checkin all files are current

---
+++ Pods/Target Support Files/Pods-MAS Sandbox/Pods-MAS Sandbox-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MAS_Sandbox : NSObject
+@end
+@implementation PodsDummy_Pods_MAS_Sandbox
+@end
---
+++ Pods/Target Support Files/Pods-MAS Sandbox/Pods-MAS Sandbox-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_MAS_SandboxVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MAS_SandboxVersionString[];
+
